61c4110b6d98be033474cf3f5f38bffd8d2661f7
Nischal Shrestha <nischalstha9@gmail.com>
2023-03-14 07:50:41 +0545

    lab3 done

---
+++ Lab3RelativeLayout/app/src/main/java/com/example/lab_3_relativelayout/main.java
@@ -0,0 +1,13 @@
+package com.example.lab_3_relativelayout;
+        import android.app.Activity;
+        import android.os.Bundle;
+
+        import androidx.annotation.Nullable;
+
+public class main extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
